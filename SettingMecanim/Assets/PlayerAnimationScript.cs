fe11c7a6d4c974f51715d20b4494cc075d7caf03
Akiko Kawai <haruka.sao@gmail.com>
2017-02-14 18:52:10 +0900

    [SettingMecanim] Added Animation Script

---
+++ SettingMecanim/Assets/PlayerAnimationScript.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerAnimationScript : MonoBehaviour
+{
+    private Animator animator;
+    private readonly String animationType = "AnimationType";
+
+	// Use this for initialization
+	void Start ()
+	{
+	    animator = GetComponent<Animator>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	    if (Input.GetKeyDown(KeyCode.RightArrow))
+	    {
+	        if (animator)
+	        {
+	            animator.SetInteger(animationType, 1);
+	        }
+	    }
+        if (Input.GetKeyDown(KeyCode.LeftArrow))
+        {
+            if (animator)
+            {
+                animator.SetInteger(animationType, 2);
+            }
+        }
+    }
+}
