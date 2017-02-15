4080e4ef27686b1ecf9ae06be7d837f38861a111
Akiko Kawai <haruka.sao@gmail.com>
2017-02-15 20:56:29 +0900

    Modified null ckeck

--- SettingMecanim/Assets/PlayerAnimationScript.cs
+++ SettingMecanim/Assets/PlayerAnimationScript.cs
@@ -8,27 +8,25 @@ public class PlayerAnimationScript : MonoBehaviour
     private Animator animator;
     private readonly String animationType = "AnimationType";
 
-	// Use this for initialization
-	void Start ()
-	{
-	    animator = GetComponent<Animator>();
-	}
-	
-	// Update is called once per frame
-	void Update () {
-	    if (Input.GetKeyDown(KeyCode.RightArrow))
-	    {
-	        if (animator)
-	        {
-	            animator.SetInteger(animationType, 1);
-	        }
-	    }
+    // Use this for initialization
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (!animator)
+            return;
+
+        if (Input.GetKeyDown(KeyCode.RightArrow))
+        {
+            animator.SetInteger(animationType, 1);
+        }
         if (Input.GetKeyDown(KeyCode.LeftArrow))
         {
-            if (animator)
-            {
-                animator.SetInteger(animationType, 2);
-            }
+            animator.SetInteger(animationType, 2);
         }
     }
 }
