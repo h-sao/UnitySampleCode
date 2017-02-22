385943ee707619cba46cb9b1c81aad074633ff92
Akiko Kawai <haruka.sao@gmail.com>
2017-02-22 19:19:00 +0900

    WIP

---
+++ SettingMecanim/Assets/Player/PlayerScript.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerScript : MonoBehaviour
+{
+
+    // Use this for initialization
+    void Start()
+    {
+        transform.position += (Vector3.forward * 10f * Time.deltaTime);
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
